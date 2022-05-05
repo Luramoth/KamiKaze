89c00e12790ab6e345a17c2861d657561951abf0
Kayla <fasreaperearnest@gmail.com>
2022-05-04 22:23:49 -0700

    set starting point for state system

--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -12,15 +12,16 @@ using UnityEngine;
 // TO/DO: possibly add double jump?
 // TODO: add dash
 // TODO: add roll
+// TODO: change movement system to state-based system
 
 public class PlayerControl : MonoBehaviour
 {
 	//vars
 	[Header("Basic movement stuff")]
-	public float speed = 6f;
+	public float walkspeed = 6f;
 	public float runSpeed = 10f;
 	public float jumpPower = 3f;
-	public float gravity = 19.62f;
+	public float baseGravity = 19.62f;
 	public bool mouseLock = true;
 	public bool isRunning = false;
 	public bool isJumping = false;
@@ -31,80 +32,26 @@ public class PlayerControl : MonoBehaviour
 	private float turnSmoothVel;
 	private Vector3 velocity;
 
-
-	//Objects
 	[Header("References")]
 	public CharacterController controller;
 	public Transform cam;
 
+	public enum moveStates {walking,running,jumping,doubleJumping,falling};
+	public moveStates moveState = moveStates.falling;
+
 	// Update is called once per frame
 	void Update()
 	{
-		ControlHandler();
+		
 	}
 
 	//simple system to handle player input
-	void ControlHandler()
+	void basicControl()
 	{
-		float charSpeed;
-
 		if (controller.isGrounded && velocity.y < 0)
 		{
 			velocity.y = -2f;
 		}
-
-		if (Input.GetKey("left shift"))
-		{
-			charSpeed = runSpeed;
-
-			isRunning = true;
-		}
-		else
-		{
-			charSpeed = speed;
-
-			isRunning = false;
-		}
-
-		//gather axis movements
-		Vector3 inputAxis = new Vector3
-		(
-			Input.GetAxisRaw("Horizontal"),
-			0f,
-			Input.GetAxisRaw("Vertical")
-		).normalized;
-
-		if (controller.isGrounded)
-		{
-			isJumping = false;
-			hasDoubleJumped = false;
-		}
-
-		//make character jump if space is pressed
-		if (Input.GetButtonDown("Jump")) 
-		{
-			if (canJump())
-			{
-				velocity.y = Mathf.Sqrt(jumpPower * -2.0f * -gravity);
-				isJumping = true;
-			}
-		}
-		velocity.y -= gravity * Time.deltaTime; //handle gravity
-
-		//apply movements if input is detected
-		if (inputAxis.magnitude >= 0.1f)
-		{
-			// based ont he players movement direction, try to rotate the player model to match it
-			float targetAngle = Mathf.Atan2(inputAxis.x,inputAxis.z) * Mathf.Rad2Deg + cam.eulerAngles.y; //this find the target angle the player should be facing in
-			float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVel, turnSmoothSpeed);// the creates a variable to make sure the transition is smooth when switching angles
-			transform.rotation = Quaternion.Euler(0f,angle,0f); // the applies the angle
-
-			// move the player
-			Vector3 moveDir = Quaternion.Euler(0f,targetAngle,0f) * Vector3.forward; // this will take the current direction the camera is facing
-			controller.Move(moveDir.normalized * charSpeed * Time.deltaTime);// this uses the direction the camera is facing in order to move forward
-		}
-
-		controller.Move(velocity * Time.deltaTime);// handle character vertical movement
 
 		//unlock cursor when escape is pressed
 		if (Input.GetButtonDown("Cancel"))
@@ -141,5 +88,54 @@ public class PlayerControl : MonoBehaviour
 				return false;
 			}
 		}
+	}
+
+	void basicMove(int speed)
+	{
+		//gather axis movements
+		Vector3 inputAxis = new Vector3
+		(
+			Input.GetAxisRaw("Horizontal"),
+			0f,
+			Input.GetAxisRaw("Vertical")
+		).normalized;
+
+		//apply movements if input is detected
+		if (inputAxis.magnitude >= 0.1f)
+		{
+			// based ont he players movement direction, try to rotate the player model to match it
+			float targetAngle = Mathf.Atan2(inputAxis.x,inputAxis.z) * Mathf.Rad2Deg + cam.eulerAngles.y; //this find the target angle the player should be facing in
+			float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVel, turnSmoothSpeed);// the creates a variable to make sure the transition is smooth when switching angles
+			transform.rotation = Quaternion.Euler(0f,angle,0f); // the applies the angle
+
+			// move the player
+			Vector3 moveDir = Quaternion.Euler(0f,targetAngle,0f) * Vector3.forward; // this will take the current direction the camera is facing
+			controller.Move(moveDir.normalized * speed * Time.deltaTime);// this uses the direction the camera is facing in order to move forward
+		}
+	}
+
+	void basicJump(float gravity)
+	{
+		if (controller.isGrounded)
+		{
+			isJumping = false;
+			hasDoubleJumped = false;
+		}
+
+		//make character jump if space is pressed
+		if (Input.GetButtonDown("Jump")) 
+		{
+			if (canJump())
+			{
+				velocity.y = Mathf.Sqrt(jumpPower * -2.0f * -gravity);
+				isJumping = true;
+			}
+		}
+	}
+
+	void basicGravity(float gravity)
+	{
+		velocity.y -= gravity * Time.deltaTime; //handle gravity
+		controller.Move(velocity * Time.deltaTime);// handle character vertical movement
 	}
 }
